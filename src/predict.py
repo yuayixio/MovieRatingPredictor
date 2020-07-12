6424654173cef901fd991239ffab49119af778ed
utehf <utehf@student.kit.edu>
2020-07-12 12:52:30 +0200

    implemented target format

--- src/predict.py
+++ src/predict.py
@@ -9,8 +9,30 @@ def predict_ratings():
     """
 
     test_data = pd.read_csv("../data/modeling/test/test.csv")
+    test_data_with_rating = pd.DataFrame(columns=["userID","movieID","rating"])
+    
+    # loop through test data
+    for index, row in test_data.iterrows():
+        
+        user_id = test_data['user_id'][index]
+        movie_id = test_data['user_id'][index]
 
-    test_data_with_rating = test_data
-    test_data_with_rating["rating"] = 3.5  # just a dummy value to get test passing
-
+        # Insert Prediction function and get results as return
+    
+        # get userID, movieID and rating -> wanted output format
+        rating = RATINGPREDICTIONFUNCTION(user_id, movie_id)
+    
+        # make use of helper function to put prediction into the right shape
+        test_data_with_rating = putRatingsIntoFormat(user_id, movie_id, rating, test_data_with_rating)
+    
     return test_data, test_data_with_rating
+
+
+############## Function to get prediction into the right shape ##########################
+
+def putRatingsIntoFormat (userID, movieID, rating, output_df = pd.DataFrame(columns=["userID","movieID","rating"])):
+    
+    ratingList = {'userID':userID, "movieID":movieID, "rating":rating}
+    output_df = output_df.append(ratingList, ignore_index=True)
+    
+    return output_df
