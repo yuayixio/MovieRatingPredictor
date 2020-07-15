693eadebc43bca11d35ee6a907124bc927669aa2
ujexa <ujexa@student.kit.edu>
2020-07-15 15:09:16 +0200

    removed main() from predict.py

--- src/predict.py
+++ src/predict.py
@@ -2,18 +2,13 @@ import pandas as pd
 
 from train import make_prediction
 
-def main():
-    test_data, ratings = predict_ratings()
-    ratings.to_csv("predicted_ratings.csv")
-
 def predict_ratings():
     """
     prediction function for assessment submission
     :return: returns test_data and test_data_with_rating
     """
     # read in test data
-    #test_data = pd.read_csv("../data/modeling/test/test.csv")
-    test_data = pd.read_csv("../data/modeling/test/ratings_testset.csv")
+    test_data = pd.read_csv("../data/modeling/test/test.csv")
 
     # read in stored movies to map movieID to unique imdbID
     movies = pd.read_csv("../data/modeling/train/movies_id_updated.csv", sep=',')
@@ -27,6 +22,3 @@ def predict_ratings():
     test_data_with_rating = test_data_with_rating[['user_id', 'movieID', 'rating']]
 
     return test_data, test_data_with_rating
-
-if __name__ == "__main__":
-    main()
